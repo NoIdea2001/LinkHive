f350a63b92ef33cca8589a28ddac5a91eb3efe7f
NoIdea2001 <saumyabhaintwal@gmail.com>
2025-09-18 13:58:03 +0530

    vercel error resloved

--- pages/api/tempLink.ts
+++ pages/api/tempLink.ts
@@ -1,19 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { NextApiRequest, NextApiResponse } from 'next'
  
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    switch (req.method) {
-        case 'GET':
-            // Handle GET requests
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
+//     switch (req.method) {
+//         case 'GET':
+//             // Handle GET requests
             
-            break
-        case 'POST':
-            // Handle POST requests
-            const inputLink = req.body.link;
+//             break
+//         case 'POST':
+//             // Handle POST requests
+//             const inputLink = req.body.link;
             
-            break
-        default:
-            // Handle other HTTP methods
-            break
-    }
+//             break
+//         default:
+//             // Handle other HTTP methods
+//             break
+//     }
   
-}
+// }
