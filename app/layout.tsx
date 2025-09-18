85d10eae6d8691dd26e94598a70f997bd07b41e7
NoIdea2001 <saumyabhaintwal@gmail.com>
2025-09-18 13:50:47 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const siteTitle = "LinkHive";
 const siteDescription = "Build and share beautiful link pages with LinkHive.";
@@ -43,6 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">{children}</body>
+      <Analytics />
     </html>
   );
 }
